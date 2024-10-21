7ca485ab70c15f821d231db399032635828880bd
Ricardo Andre Lopes Ikeda <89215809+RicardoALIkeda@users.noreply.github.com>
2024-10-21 13:00:17 +0000

    sim

---
+++ src/main/java/com/ikeda/teste/Carro.java
@@ -0,0 +1,19 @@
+package com.ikeda.teste;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Carro {
+    private String modelo;
+    private String marca;
+
+
+
+}
